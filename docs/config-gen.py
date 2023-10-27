71ba03cd0c1a09bc001971e2b61bc107a5f9569f
Vitaliy Vasylenko <me@vitalvas.com>
2023-10-26 20:49:37 -0400

    chore: update docs

---
+++ docs/config-gen.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+class ConfigGenerator:
+    def __init__(self, config: dict) -> None:
+        self.config = config
+
+    def _streaming_server(self) -> list:
+        return [
+            f"set services analytics streaming-server {self.config['streaming_server_name']} remote-address {self.config['collector_address']}",
+            f"set services analytics streaming-server {self.config['streaming_server_name']} remote-port {self.config['collector_port']}",
+        ]
+
+    def _export_profile(self) -> list:
+        data = [
+            f"set services analytics export-profile {self.config['export_profile_name']} reporting-rate {self.config['export_reporting_rate']}",
+            f"set services analytics export-profile {self.config['export_profile_name']} payload-size 1480",
+            f"set services analytics export-profile {self.config['export_profile_name']} format gpb",
+            f"set services analytics export-profile {self.config['export_profile_name']} transport udp",
+            f"set services analytics export-profile {self.config['export_profile_name']} forwarding-class network-control",
+        ]
+
+        if self.config.get('device_address'):
+            data.append(f"set services analytics export-profile {self.config['export_profile_name']} local-address {self.config['device_address']}")
+        
+        return data
+    
+    def _sensor(self) -> list:
+        resources = [
+            "/junos/system/linecard/firewall/",
+            "/junos/system/linecard/interface/",
+        ]
+
+        data = []
+
+        for resource in resources:
+            sensor_suffix = resource.replace("/", "-")[1:-1]
+            sensor_name = f"{self.config['sensor_prefix']}-{sensor_suffix}"
+
+            data.extend([
+                f"set services analytics sensor {sensor_name} server-name {self.config['streaming_server_name']}",
+                f"set services analytics sensor {sensor_name} export-name {self.config['export_profile_name']}",
+                f"set services analytics sensor {sensor_name} resource {resource}"
+            ])
+        
+        return data
+
+    def generate(self):
+        rows = []
+        rows.extend(self._streaming_server())
+        rows.extend(self._export_profile())
+        rows.extend(self._sensor())
+        print("\n".join(rows))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate config for Juniper Telemetry Streaming')
+    parser.add_argument('--collector-address', type=str, required=True)
+    parser.add_argument('--collector-port', type=int, default=21000)
+    parser.add_argument('--device-address', type=str)
+    parser.add_argument('--export-profile-name', type=str, default='jsa-export')
+    parser.add_argument('--export-reporting-rate', type=int, default=10)
+    parser.add_argument('--sensor-prefix', type=str, default='jsa')
+    parser.add_argument('--streaming-server-name', type=str, default='jsa-collector')
+    
+    args = parser.parse_args()
+
+    ConfigGenerator(config=args.__dict__).generate()
--- docs/junos.md
+++ docs/junos.md
@@ -2,28 +2,33 @@
 
 ## Configure
 
+* JTI - listen to the UDP socket and accept protobuf from juniper
+* GNMI - connecting to switches via GRPC and receiving metrics (not implemented yet)
+
+### Using JTI (Juniper Telemetry Interface)
+
 ```shell
-set services analytics streaming-server JSA-Server remote-address 192.0.2.22
-set services analytics streaming-server JSA-Server remote-port 21000
+set services analytics streaming-server jsa-collector remote-address 192.0.2.22
+set services analytics streaming-server jsa-collector remote-port 21000
 ```
 
 ```shell
-set services analytics export-profile JSA-Export local-address 192.0.2.1
-set services analytics export-profile JSA-Export reporting-rate 30
-set services analytics export-profile JSA-Export payload-size 1480
-set services analytics export-profile JSA-Export format gpb
-set services analytics export-profile JSA-Export transport udp
+set services analytics export-profile jsa-export local-address 192.0.2.1
+set services analytics export-profile jsa-export reporting-rate 10
+set services analytics export-profile jsa-export payload-size 1480
+set services analytics export-profile jsa-export format gpb
+set services analytics export-profile jsa-export transport udp
+set services analytics export-profile jsa-export forwarding-class network-control
 ```
 
 ```shell
-set services analytics sensor JSA-system-linecard-firewall server-name JSA-Server
-set services analytics sensor JSA-system-linecard-firewall export-name JSA-Export
-set services analytics sensor JSA-system-linecard-firewall resource /junos/system/linecard/firewall/
-set services analytics sensor JSA-system-linecard-firewall resource-filter uplinks.*
+set services analytics sensor jsa-system-linecard-firewall server-name jsa-collector
+set services analytics sensor jsa-system-linecard-firewall export-name jsa-export
+set services analytics sensor jsa-system-linecard-firewall resource /junos/system/linecard/firewall/
 ```
 
 ```shell
-set services analytics sensor JSA-system-linecard-interface server-name JSA-Server
-set services analytics sensor JSA-system-linecard-interface export-name JSA-Export
-set services analytics sensor JSA-system-linecard-interface resource /junos/system/linecard/interface/
+set services analytics sensor jsa-system-linecard-interface server-name jsa-collector
+set services analytics sensor jsa-system-linecard-interface export-name jsa-export
+set services analytics sensor jsa-system-linecard-interface resource /junos/system/linecard/interface/
 ```
